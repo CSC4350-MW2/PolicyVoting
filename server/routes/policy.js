0e62d13b22f49afc6d3c2fd2169d6bf438ddfc40
Paul Jr <pauljrofremu@gmail.com>
2022-11-15 18:59:48 -0500

    Add vote policy route and replace votes field in policy with accept and reject

--- server/routes/policy.js
+++ server/routes/policy.js
@@ -6,6 +6,7 @@ const {
   getAllPolicies,
   getCurrentPolicies,
   getHistory,
+  votePolicy,
 } = require("../controllers/policy");
 
 router.route("/").post(addPolicy);
@@ -13,5 +14,6 @@ router.route("/").get(getAllPolicies);
 router.route("/history").get(getHistory);
 router.route("/current").get(getCurrentPolicies);
 router.route("/:id").get(getPolicy);
+router.route("/:id/vote").post(votePolicy);
 
 module.exports = router;
